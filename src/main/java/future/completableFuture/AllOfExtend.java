8cba19011f8b0ced92a842d16c8f67c97c182086
xuchaozhen <xuchaozhen@tuhu.cn>
2022-03-22 12:50:17 +0800

    1

--- src/main/java/future/completableFuture/AllOfExtend.java
+++ src/main/java/future/completableFuture/AllOfExtend.java
@@ -2,12 +2,13 @@ package future.completableFuture;
 
 import java.util.concurrent.CompletableFuture;
 
-public class AllOfGroup {
+public class AllOfExtend {
     public static void main(String[] args) {
-        taskAfterAllOf();
+//        allOfGroup();
+        allOfException();
     }
 
-    public static void taskAfterAllOf() {
+    public static void allOfGroup() {
         CompletableFuture<Integer> task11 = CompletableFuture.supplyAsync(() -> 1);
         CompletableFuture<Integer> task12 = CompletableFuture.supplyAsync(() -> 2);
         CompletableFuture<Integer> task13 = CompletableFuture.supplyAsync(() -> 3);
@@ -25,6 +26,26 @@ public class AllOfGroup {
 
         final CompletableFuture<Integer> task31 = group2.thenApply((num) -> task21.join() + task22.join() + task23.join());
         System.out.println(task31.join());
+
+    }
+
+    public static void allOfException() {
+        CompletableFuture<Integer> task11 = CompletableFuture.supplyAsync(() -> 1);
+        CompletableFuture<Integer> task12 = CompletableFuture.supplyAsync(() -> 2);
+        CompletableFuture<Integer> task13 = CompletableFuture.supplyAsync(() -> {
+//            int i = 1/0;
+            return 3;
+        });
+
+        CompletableFuture<Void> group1 = CompletableFuture.allOf(task11, task12, task13);
+        group1.whenComplete((num,e) -> {
+            if(e != null){
+                System.out.println("发生异常");
+                System.out.println(e.getMessage());
+            }else {
+                System.out.println(task11.join() + task12.join() + task13.join());
+            }
+        });
 
     }
 }
